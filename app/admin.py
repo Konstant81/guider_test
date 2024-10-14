e76c729723a3d134f9d2621ca39da2dcea896cc5
Konstantin Ilyushin <radist1981@mail.ru>
2024-10-14 12:09:26 +0300

    registes models in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import City, Street, Shop
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(Street)
+admin.site.register(Shop)
