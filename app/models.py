63ce7d6eb49b07d320077ff20b62893ae46ee300
Konstantin Ilyushin <radist1981@mail.ru>
2024-10-14 12:07:14 +0300

    create models, serializers, views

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = title = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "city"                   
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
+
+class Street(models.Model):
+    name = title = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='город')
+
+
+    class Meta:
+        db_table = "street"
+        verbose_name = "Улица"
+        verbose_name_plural = "Улицы"
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='город')
+    street =  models.ForeignKey(Street, on_delete=models.CASCADE, verbose_name='улица')
+    house_number = models.IntegerField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    class Meta:
+        db_table = "shop"
+        verbose_name = "Магазин"
+        verbose_name_plural = "Магазины"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CitySerializer, StreetSerializer, ShopSerializer
+from .models import City, Street, Shop
 
-# Create your views here.
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+class StreetViewSet(viewsets.ModelViewSet):
+    queryset = Street.objects.all()
+    serializer_class = StreetSerializer()
+
+class ShopViewSet(viewsets.ModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
